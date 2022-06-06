532d58f041ce2b31b81a324aead9351ac29f293c
Erdal256 <erdall.2693@gmail.com>
2022-06-06 10:35:51 +0300

    first commit

---
+++ EntityLayer/Concrete/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Category
+    {
+        //Erişim belirleyici türü - Değişken tür isim {get set}
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }    
+        public string CategoryDescription { get; set; }         
+        public bool CategoryStatus{ get; set; } //Category türlerini aktif veya pasif yapmak için bool kullanılır
+
+    }
+}
